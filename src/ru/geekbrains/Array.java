92c2957644ec0a69f903329eb7fa531e5ed39922
Ekaterina Malygina <katty2007v@yandex.ru>
2021-01-17 11:00:11 +0300

    hw algorithms lesson-2

--- src/ru/geekbrains/Array.java
+++ src/ru/geekbrains/Array.java
@@ -33,6 +33,8 @@ public class Array {
     }
 
     public void set(int index, int value) {
+        if (index >= size || index < 0)
+            throw new ArrayIndexOutOfBoundsException("Your index is not correct: " + index);
         arr[index] = value;
         isSorted = false;
     }
@@ -101,46 +103,134 @@ public class Array {
     }
 
     public void sortBubble() {
+        int count = 0;
         for (int out = size - 1; out > 0; out--) {
+            boolean sorted = true;
             for (int in = 0; in < out; in++) {
+                count++;
                 if (this.arr[in] > arr[in + 1]) {
                     swap(in, in + 1);
+                    sorted = false;
                 }
+            }
+            if (sorted) {
+                break;
             }
         }
         isSorted = true;
+        System.out.println("sortBubble count: " + count + ", n = " + size);
     }
 
     public void sortSelect() {
+        int count = 0;
         for (int i = 0; i < size; i++) {
             int flag = i;
             for (int j = i + 1; j < size; j++) {
+                count++;
                 if (arr[j] < arr[flag])
                     flag = j;
             }
             swap(i, flag);
         }
         isSorted = true;
+        System.out.println("sortSelect count: " + count + ", n = " + size);
     }
 
     public void sortInsert() {
+        int count = 0;
         for (int out = 1; out < size; out++) {
             int temp = arr[out];
             int in = out;
+            count++;
             while (in > 0 && arr[in - 1] >= temp) {
+                count++;
                 arr[in] = arr[in - 1];
                 in--;
             }
             arr[in] = temp;
         }
         isSorted = true;
+        System.out.println("sortInsert count: " + count + ", n = " + size);
     }
 
-    // homework
-//        boolean deleteAll(int value) { } // by value
-//        boolean deleteAll() { } // clear
-//        void insert(int index, int value) { } // shift the tail
-//        // improve bubble sort (kinda make it better somehow)
+    public boolean deleteAll(int value) {
+        if (this.isSorted) {
+            return this.deleteAllSorted(value);
+        } else {
+            boolean res = false;
+            int shift = 0;
+            for (int i = 0; i < size; i++) {
+                if (this.arr[i] == value) {
+                    res = true;
+                    shift++;
+                } else {
+                    this.arr[i - shift] = this.arr[i];
+                }
+            }
+            size = size - shift;
+            return res;
+        }
+    }
+
+    public boolean deleteAllSorted(int value) {
+        int i = this.hasValue(value);
+        if (i == -1) {
+            return false;
+        } else {
+            int iMin = i;
+            int iMax = i;
+            while ((iMin - 1) >= 0 && this.arr[iMin - 1] == value) {
+                iMin = iMin - 1;
+            }
+            while ((iMax + 1) < size && this.arr[iMax + 1] == value) {
+                iMax = iMax + 1;
+            }
+            System.arraycopy(arr, iMax + 1, arr, iMin, size - iMax - 1);
+            size = size - (iMax - iMin) - 1;
+        }
+        return true;
+    }
+
+    public boolean deleteAll() {
+        if (this.size == 0) {
+            return false;
+        }
+        this.size = 0;
+        this.arr = new int[0];
+        return true;
+    }
+
+    public void insert(int index, int value) {
+        if (index >= size || index < 0)
+            throw new ArrayIndexOutOfBoundsException("Your index is not correct: " + index);
+        if (size >= arr.length - 1) {
+            int[] temp = arr;
+            arr = new int[size * 2];
+            System.arraycopy(temp, 0, arr, 0, index);
+            System.arraycopy(temp, index, arr, index + 1, size - index);
+            size++;
+            arr[index] = value;
+        } else {
+            System.arraycopy(arr, index, arr, index + 1, size - index);
+            size++;
+            arr[index] = value;
+        }
+    }
+
+    //* Реализовать сортировку подсчётом
+    public void sortCount(int min, int max) {
+        int[] arrCount = new int[max - min + 1];
+        for (int i = 0; i < size; i++) {
+            arrCount[arr[i] - min]++;
+        }
+        int shift = 0;
+        for (int i = 0; i < arrCount.length; i++) {
+            for (int k = shift; k < shift + arrCount[i]; k++) {
+                arr[k] = i + min;
+            }
+            shift = shift + arrCount[i];
+        }
+    }
 
     @Override
     public String toString() {
--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -3,15 +3,37 @@ package ru.geekbrains;
 public class Main {
 
     public static void main(String[] args) {
-        Array array = new Array(9,2,3,7,4,5,6,7,1,8,0);
+        Array array = new Array(1,2,3,4,5,6,7,9,8,10);
         System.out.println(array);
+        //array.sortBubble();
+        //array.sortSelect();
+        array.sortInsert();
+        System.out.println(array);
+        Array array2 = new Array(1,9,3,4,3,6,7,8,2,0,13,15,16,21,23,14,11,18,3,10,43,3);
+        System.out.println(array2);
+        //array2.sortBubble();
+        //array2.sortSelect();
+        //array2.sortInsert();
+        System.out.println(array2);
+        array2.deleteAll(3);
+        System.out.println(array2);
+        array2.insert(2, 13);
+        System.out.println(array2);
+        Array array3 = new Array(-6,-3,-2,-3,-6,-1,-1,0,0,-3,-2);
+        System.out.println(array3);
+        array3.sortCount(-6,0);
+        System.out.println(array3);
+        Array array4 = new Array(true,1);
+        array4.deleteAllSorted(1);
+//        array2.deleteAll();
+//        System.out.println(array2);
 //        array.delete();
 //        array.delete();
 //        System.out.println(array);
 //        array.delete(2);
 //        System.out.println(array);
-        array.sortInsert();
-        System.out.println(array);
-        System.out.println(array.hasValue(7));
+//        array.sortInsert();
+//        System.out.println(array);
+//        System.out.println(array.hasValue(7));
     }
 }
